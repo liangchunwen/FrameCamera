92ef5e13d2ad15a958d389001a72ae6b29d7562b
liangcw <892534075@qq.com>
2021-09-17 17:50:31 +0800

    [Use new recording voice for Recording]
    	modified:   app/src/main/java/com/frame/camera/utils/CameraSoundUtils.java
    	new file:   app/src/main/res/raw/start_video_rec.mp3
    	new file:   app/src/main/res/raw/stop_video_rec.mp3

--- app/src/main/java/com/frame/camera/utils/CameraSoundUtils.java
+++ app/src/main/java/com/frame/camera/utils/CameraSoundUtils.java
@@ -1,11 +1,15 @@
 package com.frame.camera.utils;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 
+import com.frame.camera.R;
+import com.frame.camera.sound.ISoundPlayback;
 import com.frame.camera.sound.SoundPlaybackImpl;
 
 public class CameraSoundUtils {
     private static SoundPlaybackImpl mSoundPlayback;
+    private static MediaPlayer mediaPlayer;
 
     public static void initSound(Context context) {
         if (mSoundPlayback == null) {
@@ -14,10 +18,33 @@ public class CameraSoundUtils {
     }
 
     public static void playSound(Context context, int sound) {
-        if (mSoundPlayback == null) {
-            initSound(context);
+        if (sound == ISoundPlayback.START_VIDEO_RECORDING) {
+            playRecordingSound(context, R.raw.start_video_rec);
+        } else if (sound == ISoundPlayback.STOP_VIDEO_RECORDING) {
+            playRecordingSound(context, R.raw.stop_video_rec);
+        } else {
+            if (mSoundPlayback == null) {
+                initSound(context);
+            }
+            mSoundPlayback.play(sound);
         }
-        mSoundPlayback.play(sound);
+    }
+
+    private static void playRecordingSound(Context context, int raw) {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        mediaPlayer = MediaPlayer.create(context, raw);
+        mediaPlayer.start();
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mediaPlayer.release();
+                mediaPlayer = null;
+            }
+        });
     }
 
     public static void releaseSound() {
