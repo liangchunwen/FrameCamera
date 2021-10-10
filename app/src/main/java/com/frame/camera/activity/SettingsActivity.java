b3262d4ae6d138b81b70264486e5344732f6d02a
liangcw <892534075@qq.com>
2021-10-10 12:05:19 +0800

    [添加视频拼接代码及优化设置菜单]
    	modified:   app/build.gradle
    	modified:   app/src/main/java/com/frame/camera/activity/SettingsActivity.java
    	new file:   app/src/main/java/com/frame/camera/utils/JoinVideoUtils.java
    	modified:   app/src/main/res/layout/fragment_camera.xml
    	new file:   app/src/main/res/values-zh-rCN/arrays.xml
    	modified:   app/src/main/res/values-zh-rCN/strings.xml
    	modified:   app/src/main/res/values/arrays.xml
    	modified:   app/src/main/res/values/strings.xml
    	modified:   app/src/main/res/xml/root_preferences.xml

--- app/src/main/java/com/frame/camera/activity/SettingsActivity.java
+++ app/src/main/java/com/frame/camera/activity/SettingsActivity.java
@@ -1,15 +1,20 @@
 package com.frame.camera.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
-
+import androidx.preference.Preference.OnPreferenceChangeListener;
 import com.frame.camera.R;
+import com.frame.camera.application.MyApplication;
 
 public class SettingsActivity extends AppCompatActivity {
+    private static final String TAG = "SettingsActivity:CAMERA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +44,44 @@ public class SettingsActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
+    public static class SettingsFragment extends PreferenceFragmentCompat implements OnPreferenceChangeListener{
+        private static final String KEY_PRETRAN = "pre_transcription";
+        private static final String KEY_PROTEIN = "protein";
+        private ListPreference preTranListPre;
+        private ListPreference proteinListPre;
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            preTranListPre = findPreference(KEY_PRETRAN);
+            if (preTranListPre != null) {
+                String val = MyApplication.mSharedPreferences.getString("pre_transcription_values", "0");
+                Log.d(TAG, "pre-val: " + val);
+                preTranListPre.setValue(val);
+                preTranListPre.setOnPreferenceChangeListener(this);
+            }
+            proteinListPre = findPreference(KEY_PROTEIN);
+            if (proteinListPre != null) {
+                String val = MyApplication.mSharedPreferences.getString("protein_values", "0");
+                Log.d(TAG, "pro-val: " + val);
+                proteinListPre.setValue(val);
+                proteinListPre.setOnPreferenceChangeListener(this);
+            }
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String value = (String) newValue;
+            Log.d(TAG, "onPreferenceChange-value: " + value);
+            if (preference.getKey().equals(KEY_PRETRAN)) {
+                preTranListPre.setValue(value);
+                MyApplication.mEditor.putString("pre_transcription_values", value).apply();
+            } else if (preference.getKey().equals(KEY_PROTEIN)) {
+                proteinListPre.setValue(value);
+                MyApplication.mEditor.putString("protein_values", value).apply();
+            }
+
+            return false;
         }
     }
 }
---
+++ app/src/main/java/com/frame/camera/utils/JoinVideoUtils.java
@@ -0,0 +1,136 @@
+package com.frame.camera.utils;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.Track;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
+import com.googlecode.mp4parser.authoring.tracks.AppendTrack;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by pengshulin on 2017/4/28.
+ */
+
+public class JoinVideoUtils {
+    private static final String TAG = "JoinVideoUtils_TAG";
+    private final Context context;
+    //需要拼接的mp4视频的地址
+    private final List<String> videoUris;
+    //合并完毕，导出地址
+    private final String output;
+    //是否正在执行合并任务
+    private boolean isRunning = false;
+
+    public JoinVideoUtils(Context context, List<String> videoUris, String output) {
+        this.context = context;
+        this.videoUris = videoUris;
+        this.output = output;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void joinVideo() {
+        isRunning = true;
+
+        if (videoUris == null || videoUris.size() <= 0 || TextUtils.isEmpty(output)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (videoUris.size() == 1) { // 只有一个视频片段，不需要合并
+            return;
+        }
+
+        try {
+
+            List<Movie> inMovies = new ArrayList<>();
+
+            for (String videoUri : videoUris) {
+                File f = new File(videoUri);
+                //文件存在
+                if (f.exists()) {
+                    inMovies.add(MovieCreator.build(videoUri));
+                }
+            }
+
+            // 分别取出音轨和视频
+            List<Track> videoTracks = new LinkedList<>();
+            List<Track> audioTracks = new LinkedList<>();
+            for (Movie m : inMovies) {
+                for (Track t : m.getTracks()) {
+                    if (t.getHandler().equals("soun")) {
+                        audioTracks.add(t);
+                    }
+                    if (t.getHandler().equals("vide")) {
+                        videoTracks.add(t);
+                    }
+                }
+            }
+
+            // 合并到最终的视频文件
+            Movie outMovie = new Movie();
+
+            if (!audioTracks.isEmpty()) {
+                Track[] tracks = new Track[audioTracks.size()];
+                outMovie.addTrack(new AppendTrack(audioTracks.toArray(tracks)));
+            }
+            if (!videoTracks.isEmpty()) {
+                Track[] tracks = new Track[videoTracks.size()];
+                outMovie.addTrack(new AppendTrack(videoTracks.toArray(tracks)));
+            }
+
+            Container mp4file = new DefaultMp4Builder().build(outMovie);
+
+            // 将文件输出
+            File resultFile = new File(output);
+
+            //删除resultFile
+            if (resultFile.exists() && resultFile.isFile()) {
+                Log.d(TAG, "delete old file before output the new file!" + resultFile.delete());
+            }
+
+            FileChannel fc = new RandomAccessFile(resultFile, "rw").getChannel();
+            mp4file.writeContainer(fc);
+            fc.close();
+
+            // 合成完成后把原片段文件删除
+            /*
+             *
+            for (String filePath : filePaths) {
+                File file = new File(filePath);
+                file.delete();
+            }
+            */
+
+            isRunning = false;
+
+            Toast.makeText(context, "拼接成功", Toast.LENGTH_SHORT).show();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.i(TAG, "--文件到不到异常--");
+            Log.d(TAG, "FileNotFoundException-e: " + e);
+            isRunning = false;
+            Toast.makeText(context, "拼接失败", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, "Exception-e: " + e);
+            isRunning = false;
+            Toast.makeText(context, "拼接失败", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
